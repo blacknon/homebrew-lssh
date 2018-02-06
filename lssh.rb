af95433be47908f8c6461d1bb6d4cc3f3c116880
blacknon <blacknon@orebibou.com>
2018-02-06 21:01:35 +0900

    first

---
+++ lssh.rb
@@ -0,0 +1,15 @@
+class Lssh < Formula
+  desc "List select formula ssh wrapper for terminal UI"
+  homepage "https://github.com/blacknon/lssh"
+  if OS.mac?
+    url "https://github.com/blacknon/lssh/releases/download/v0.4.2/lssh_0.4.2_darwin_amd64.tar.gz"
+    sha256 "33a5ae449b55b2a28202571077852bb34f16ce2e4c909aedf6b9d4fb44e4c2d7"
+  end
+  def install
+    bin.install Dir['lssh']
+    home.install Dir['.lssh.conf']
+  end
+  test do
+    system "#{bin}/lssh"
+  end
+end
