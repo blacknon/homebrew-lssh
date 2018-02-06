c2beb5ddd0677fe04073600e2dafd52e2436cefa
blacknon <blacknon@orebibou.com>
2018-02-06 21:39:23 +0900

    test

--- lssh.rb
+++ lssh.rb
@@ -7,8 +7,8 @@ class Lssh < Formula
   end
   def install
     bin.install Dir['lssh']
-    system "echo ~/.lssh.conf"
-    system "curl -L -o ~/.lssh.conf https://raw.githubusercontent.com/blacknon/lssh/master/example/config.tml"
+    system "echo $HOME/.lssh.conf"
+    system "curl -s -L -o ~/.lssh.conf https://raw.githubusercontent.com/blacknon/lssh/master/example/config.tml"
   end
   test do
     system "#{bin}/lssh"
