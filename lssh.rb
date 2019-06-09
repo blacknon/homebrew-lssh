00982c576f2d14d9a0ab889489427f4af43e9bca
Blacknon <blacknon@orebibou.com>
2019-06-09 22:13:13 +0900

    v0.5.5

--- lssh.rb
+++ lssh.rb
@@ -2,8 +2,8 @@ class Lssh < Formula
   desc "List select formula ssh wrapper for terminal UI"
   homepage "https://github.com/blacknon/lssh"
   if OS.mac?
-    url "https://github.com/blacknon/lssh/releases/download/v0.5.4/lssh_0.5.4_darwin_amd64.tar.gz"
-    sha256 "5b58a4d0e32c10ddd8f5b5ae86365081805c5acf774ba6c41104adfc07cb96c0"
+    url "https://github.com/blacknon/lssh/releases/download/v0.5.4/lssh_0.5.5_darwin_amd64.tar.gz"
+    sha256 "4f0f17fe8b166165525ea78cbaa650f9de776fc13136ae11b8686bdb"
   end
   def install
     bin.install Dir['lssh','lscp']
