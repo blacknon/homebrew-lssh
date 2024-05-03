1cbc3b54eedcc562ae4f3e476eb99ddfb73ae776
blacknon <blacknon@orebibou.com>
2024-05-03 23:06:41 +0900

    update. version 0.6.8

--- lssh.rb
+++ lssh.rb
@@ -1,10 +1,20 @@
 class Lssh < Formula
   desc "List select formula ssh wrapper for terminal UI"
   homepage "https://github.com/blacknon/lssh"
-  if OS.mac?
-    url "https://github.com/blacknon/lssh/releases/download/v0.6.4/lssh_0.6.4_darwin_amd64.tar.gz"
-    sha256 "46d74c6f00689d4e44aa6b3adeae2e95ab230548077458b99f8379ca4c944e32"
+  version 0.6.8
+
+  # MacOS & Apple Silicon CPU
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_arm64.tar.gz"
+    sha256 "3b2f46864f5d6624bc7a76f2f4d60939d4fc32ec0b49776f9d5735f672289df6"
   end
+
+  # MacOS & Intel CPU
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_amd64.tar.gz"
+    sha256 "74d8840a32f72057596e8248514fcd1f4ea6260912b99c1616fa62140cb271b3"
+  end
+
   def install
     bin.install Dir['lssh','lscp','lsftp']
     zsh_completion.install "zsh/_lssh"
