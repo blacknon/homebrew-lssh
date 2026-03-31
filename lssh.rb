6698f3032ffa5f89d6b3be620f2295d8f4483514
Blacknon <blacknon@orebibou.com>
2026-04-01 02:23:35 +0900

    update.

--- lssh.rb
+++ lssh.rb
@@ -1,18 +1,18 @@
 class Lssh < Formula
   desc "List select formula ssh wrapper for terminal UI"
   homepage "https://github.com/blacknon/lssh"
-  version 0.6.8
+  version 0.6.14
 
   # MacOS & Apple Silicon CPU
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_arm64.tar.gz"
-    sha256 "3b2f46864f5d6624bc7a76f2f4d60939d4fc32ec0b49776f9d5735f672289df6"
+    sha256 "3349b7d43fb1abee0c58907e846d3a1b4a3733f08da6bd169ad62052a3cf8669"
   end
 
   # MacOS & Intel CPU
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_amd64.tar.gz"
-    sha256 "74d8840a32f72057596e8248514fcd1f4ea6260912b99c1616fa62140cb271b3"
+    sha256 "39a2029a90e677e2f63396eaf49e41acf23dc2012112af293afbad6b07e64b11"
   end
 
   def install
