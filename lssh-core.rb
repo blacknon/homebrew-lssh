0caef95814b534f0e9a85b888df53104d896c0a0
Blacknon <blacknon@orebibou.com>
2026-04-06 22:49:47 +0900

    update v0.7.1

--- lssh-core.rb
+++ lssh-core.rb
@@ -1,18 +1,18 @@
 class LsshCore < Formula
   desc "Core lssh binary"
   homepage "https://github.com/blacknon/lssh"
-  version "0.7.0"
+  version "0.7.1"
 
   conflicts_with "lssh", because: "both install the lssh binary"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_arm64.tar.gz"
-    sha256 "e0238e6f8b90c05a106de9ba273be5b0c5acfc317a4825c091d94563206d251b"
+    sha256 "abe314535f99fb1c95b0fa95fe6371d99db8fa3b972130171fc4527d1d3fc69a"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/blacknon/lssh/releases/download/v#{version}/lssh_#{version}_darwin_amd64.tar.gz"
-    sha256 "d0c0f0b266ce6b68ba05506ccacf0b96041ee331242059c454594a44ab235029"
+    sha256 "3ad68f31e6dedd38e908ecbbd1ea23edc15f093b8a18ddfd76dc701b51cfe158"
   end
 
   def install
